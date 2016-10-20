0b54ae251ff3bd6060b70d84e5c9e13327d83d5f
Chris Wingo and Quinn Schwartz <chris@>
2016-10-20 09:16:09 -0700

    create html css and js files and write basic structure for page.

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  //UI Logic
+
+  var response = prompt("What animal would you like to learn more about? (Enter snakes, insects, or turtles:)");
+  //Business Logic
+
+  if (response === snakes){
+
+  } else if (response === insects){
+
+  } else {
+
+  }
+});
