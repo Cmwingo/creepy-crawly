baf283e3d9b0c0b37c9a888c682fb1b8f2c85341
Chris Wingo and Quinn Schwartz <quinn@>
2016-10-20 09:35:33 -0700

    add business logic to hide and show different divs depending on user input

--- js/scripts.js
+++ js/scripts.js
@@ -3,12 +3,14 @@ $(document).ready(function(){
 
   var response = prompt("What animal would you like to learn more about? (Enter snakes, insects, or turtles:)");
   //Business Logic
-
-  if (response === snakes){
-
-  } else if (response === insects){
-
+  if (response === "snakes"){
+    $("#snakes").toggle();
+    $('h1').text("Snakes");
+  } else if (response === "insects"){
+    $("#insects").toggle();
+    $('h1').text("Insects");
   } else {
-
+    $("#turtles").toggle();
+    $('h1').text("Turtles");
   }
 });
